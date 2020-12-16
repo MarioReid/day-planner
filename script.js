6fc100089200018585123e301ea3aafd492f620c
Mario Reid <mario.reid83@gmail.com>
2020-12-16 17:55:25 -0500

    created element for row

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ var todaysDate = $("#currentDay");
 todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 
 
-var timeOfDay = ["1", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9"];
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
+//create an element
+var row = $("<div>);
+row.addClass("row time-block");
+var hour = $("<div>");
+$(".container").append (row);
 
