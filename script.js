b534956c741f3b0d13a78281e33d568e77d3c6b3
Mario Reid <mario.reid83@gmail.com>
2020-12-16 18:01:40 -0500

    working on var for row contents

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 //create an element
-var row = $("<div>);
+var row = $("<div>");
 row.addClass("row time-block");
+
 var hour = $("<div>");
 $(".container").append (row);
+hour.addClass("hour col-1")
+row.append(hour);
+
+var textArea = $("<textarea>");
+textArea.addClass("past description col-10");
+row.append(textArea)
+
+var button = $("<button>");
+button.addClass("saveBtn col-1");
+row.append(button)
+
 
