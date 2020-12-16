947e2eb7d7e36036c95da912f633aaac756ec831
Mario Reid <mario.reid83@gmail.com>
2020-12-16 17:49:07 -0500

    made js and added todays date

---
+++ script.js
@@ -0,0 +1,8 @@
+//set today date
+var todaysDate = $("#currentDay");
+todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
+
+
+var timeOfDay = ["1", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9", "9"];
+
+
