62f4d81c93ff55fec151f53901c3c1377b861e68
Mario Reid <mario.reid83@gmail.com>
2020-12-17 15:55:00 -0500

    fixed time for the rows

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 //set today date
 var todaysDate = $("#currentDay");
+
+//sets date
 todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 
+var timeOfDay = [
+    {twelveHour: "9AM", twentyFourHour: "9AM"},
+    {twelveHour: "10AM", twentyFourHour: "10AM"},
+    {twelveHour: "11AM", twentyFourHour: "11AM"},
+    {twelveHour: "12PM", twentyFourHour: "12PM"},
+    {twelveHour: "1PM", twentyFourHour: "13PM"},
+    {twelveHour: "2PM", twentyFourHour: "14PM"},
+    {twelveHour: "3PM", twentyFourHour: "15PM"},
+    {twelveHour: "4PM", twentyFourHour: "16PM"},
+    {twelveHour: "5PM", twentyFourHour: "17PM"}];
+    
 
-//Time of day for loop
-for (var i=0; i<timeOfDay.length; i++)
-    car
-
-    if (timeOfDay[i] === moment().hour()){
-        console.log("Time is present")
-    };
+   //if (timeOfDay[i] === moment().hour()){
+    //    console.log("Time is present")
+    //};
    
 
-var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-
+//sets time
+var currentTime = moment().format("H");
 
 for (i = 0; i < timeOfDay.length; i++){
-
+    console.log("The row is being created.")
 //create row
 var row = $("<div>");
 row.addClass("row time-block");
@@ -24,7 +33,7 @@ row.addClass("row time-block");
 var hour = $("<div>");
 $(".container").append (row);
 hour.addClass("hour col-1");
-hour.text(timeOfDay[i]);
+hour.text(timeOfDay[i].twelveHour);
 row.append(hour);
 //create text area column
 var textArea = $("<textarea>");
@@ -32,10 +41,9 @@ textArea.addClass("past description col-10");
 row.append(textArea);
 //create button
 var button = $("<button>");
-button.addClass("saveBtn col-1");
+button.text
+button.addClass("saveBtn col-1 fal fa-save");
 row.append(button);
 
 $(".container").append(row);
-
 }
-
