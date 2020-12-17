19fe9ac1fedd568868b3763183aa49d99eeb1569
Mario Reid <mario.reid83@gmail.com>
2020-12-17 10:09:20 -0500

    updated

--- script.js
+++ script.js
@@ -3,23 +3,39 @@ var todaysDate = $("#currentDay");
 todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 
 
+//Time of day for loop
+for (var i=0; i<timeOfDay.length; i++)
+    car
+
+    if (timeOfDay[i] === moment().hour()){
+        console.log("Time is present")
+    };
+   
+
 var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-//create an element
+
+for (i = 0; i < timeOfDay.length; i++){
+
+//create row
 var row = $("<div>");
 row.addClass("row time-block");
-
+//create hour column
 var hour = $("<div>");
 $(".container").append (row);
-hour.addClass("hour col-1")
+hour.addClass("hour col-1");
+hour.text(timeOfDay[i]);
 row.append(hour);
-
+//create text area column
 var textArea = $("<textarea>");
 textArea.addClass("past description col-10");
-row.append(textArea)
-
+row.append(textArea);
+//create button
 var button = $("<button>");
 button.addClass("saveBtn col-1");
-row.append(button)
+row.append(button);
 
+$(".container").append(row);
+
+}
 
